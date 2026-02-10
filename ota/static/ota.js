d7343865c96cb28b0984d6eb0b1a419546f3605c
Devin March <devin@devinmarch.com>
2026-02-10 17:54:32 -0500

    50 25 25

--- ota/static/ota.js
+++ ota/static/ota.js
@@ -132,7 +132,7 @@ function renderAvailability() {
         <strong>Availability for a ${nights} night stay:</strong>
         <table style="width:100%; margin-top:8px; border-collapse:collapse;">
             <tr style="text-align:left; border-bottom:1px solid #ccc;">
-                <th style="width:60%">Room</th><th style="width:20%">Available</th><th style="width:20%">OTA Rate</th>
+                <th style="width:50%">Room</th><th style="width:25%">Available</th><th style="width:25%">OTA Rate</th>
             </tr>
             ${html}
         </table>`;
